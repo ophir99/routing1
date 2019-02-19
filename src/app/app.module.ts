549c865eadee41beb4cd1c6cc31b4e3d5d9231ae
Prasad Kadiyala <external.pvkadiyala@atheerair.com>
2019-02-19 21:42:46 +0530

    Cool

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,13 +2,33 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
-
+import { HomeComponent } from './home/home.component';
+import { AboutusComponent } from './aboutus/aboutus.component';
+import { ServiceComponent } from './service/service.component';
+import { RouterModule } from "@angular/router";
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HomeComponent,
+    AboutusComponent,
+    ServiceComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    RouterModule.forRoot([
+      {
+        path: "",
+        component: HomeComponent
+      },
+      {
+        path: "aboutus",
+        component: AboutusComponent
+      },
+      {
+        path: "contactus",
+        component: ServiceComponent
+      }
+    ])
   ],
   providers: [],
   bootstrap: [AppComponent]
